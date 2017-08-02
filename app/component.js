7cca3aa84e75164e047e4d0f8ea930f1f07e48f8
Dan Riding <doonyby@gmail.com>
2017-08-02 13:12:28 -0600

    name change test

--- app/component.js
+++ app/component.js
@@ -1,7 +1,8 @@
-export default (text = 'Hello World I am here! Are you? Anyone? Buler?') => {
+export default (text = 'Hello World I am here! Are you? Anyone? Buler? San Augustos?') => {
     const element = document.createElement('div');
 
     element.innerHTML = text;
 
     return element;
 };
+
