510ccbbc3aef575c366e5a70ace8f5fda3e86e4c
Dan Riding <doonyby@gmail.com>
2017-08-30 15:47:10 -0600

    changed entry to react but have not installed babel loaders yet

--- app/component.js
+++
@@ -1,8 +0,0 @@
-export default (text = 'Hello World I am here! Are you? Anyone? Buler? San Augustos?') => {
-    const element = document.createElement('div');
-
-    element.innerHTML = text;
-
-    return element;
-};
-
--- app/index.js
+++ app/index.js
@@ -1,3 +1,17 @@
-import component from './component';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, browserHistory } from "react-router";
 
-document.body.appendChild(component());
+import Entry from './component/Entry';
+
+class App extends React.Component {
+	render() {
+		return (
+			<Router history={browserHistory}>
+				<Route path="/" component={Entry} />
+			</Router>
+		);
+	}
+}
+
+render(<App />, document.getElementById('app'));
